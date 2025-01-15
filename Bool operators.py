036bb521577a102cb7eaccafa0de809a186fcffb
luccacampagnani <lcampagnani.ieu2023@student.ie.edu>
2025-01-15 18:53:04 +0100

    arithmetic and boolean operators

---
+++ Bool operators.py
@@ -0,0 +1,17 @@
+print(not True)
+print(not False)
+print(not 7) # False bc 7 is true
+print(True or False)
+print(True or False)
+print(2 or 3 or 6) # or prints always the first one if it is true
+print(False or 0 or 4 or True)
+print(1 and 2 and 3) # and prints always the last one if it is true
+print(1 and 0 and 4) # prints 0 bc it is not true
+print(False or 0 or None)
+
+
+
+
+
+
+
---
+++ arythemtic operations.py
@@ -0,0 +1,10 @@
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5) # always produces a float result
+print(20//5) # this is the integer division
+print(22%5) # this is the remainder of the division
+print(2**3) # this is two to the power of three
+# All of these are the arithmetic operators
+
+
